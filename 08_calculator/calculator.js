4aa58c686943222257ed54f3cd883adc3c207450
VR <vitrayev@gmail.com>
2021-09-06 17:10:57 -0400

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  if (num.length > 0) {
+    const sum = num.reduce(myFunction);
+    function myFunction(total, value) {
+      return total + value;
+    } return sum;
+  } else
+    return num.length;
+};
+const multiply = function (num) {
+  let x = num.reduce(function (product, value) {
+    return product * value;
+  }); return x;
 };
 
-const multiply = function() {
-
+const power = function (num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0)
+    return -1;
+  else if (num === 0)
+    return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 module.exports = {
