a227451643d73014dff88e631fe0e1d04c74d714
VR <vitrayev@gmail.com>
2021-09-06 14:13:46 -0400

    completed 06_leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0){
+        if (year % 400 === 0){
+            return true;
+        }else if (year % 100 === 0){
+            return false;
+        }else return true;
+    }else return false;
 };
-
 module.exports = leapYears;
