c1bd9d499dee6ab95b5afe8f2c6a64b466e167ed
VR <vitrayev@gmail.com>
2021-09-06 14:40:41 -0400

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius = ((temp - 32) * (5/9));
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fah = ((temp * (9/5)) + 32);
+  return Math.round(fah * 10) / 10;
 };
 
 module.exports = {
