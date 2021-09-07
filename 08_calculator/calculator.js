efd0046e6198ca9d973d552da7b628e0030682e8
VR <vitrayev@gmail.com>
2021-09-07 13:52:00 -0400

    completed 09_palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,9 @@
-const add = function(num1, num2) {
-	return (num1 + num2);
+const add = function (num1, num2) {
+  return (num1 + num2);
 };
 
-const subtract = function(num1, num2) {
-	return (num1 - num2);
+const subtract = function (num1, num2) {
+  return (num1 - num2);
 };
 
 const sum = function (num) {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    words = words.replace(/[^\w\s]|_/g, "")
+                 .replace(/\s+/g, "").toLowerCase();
+    let wArray = words.split('');
+    let rArray = words.split('').reverse();
+    if (wArray.every((val, index) => val === rArray[index])){
+        return true;
+    }else return false;
 };
 
 module.exports = palindromes;
