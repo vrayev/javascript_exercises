e45da4c389e032147aebd0d3f43ceb9f49eff27b
VR <vitrayev@gmail.com>
2021-09-12 14:16:48 -0400

    completed fibonacci test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (num === 0){
+        return 0;
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < num; i++){
+        const sum = b;
+        b = a + b;
+        a = sum;
+    }
+    return b;
 };
 
 module.exports = fibonacci;
