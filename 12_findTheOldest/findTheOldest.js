a565e7cda998628082911724f529bec601c45706
VR <vitrayev@gmail.com>
2021-09-12 17:56:04 -0400

    completed findTheOldest test, cleaned up code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,25 +1,25 @@
-const findTheOldest = function(people) {
+const findTheOldest = function (people) {
     const birthYear = people.map(people => people.yearOfBirth);
     const deathYear = people.map(people => people.yearOfDeath);
     let age = new Array;
     let oldest;
     const currentYear = new Date().getFullYear();
-    for(i = 0; i < birthYear[i]; i++){
-        if (typeof deathYear[i] === 'undefined'){
+    for (i = 0; i < birthYear[i]; i++) {
+        if (typeof deathYear[i] === 'undefined') {
             age.push(currentYear - birthYear[i]);
-        }else
-        age.push(deathYear[i] - birthYear[i]);
+        } else
+            age.push(deathYear[i] - birthYear[i]);
     };
-    if (age[0] && age[1] < age[2]){
+    if (age[0] && age[1] < age[2]) {
         oldest = people[2];
-        return oldest;
-    } else if (age[1] && age[2] < age[0]){
+
+    } else if (age[1] && age[2] < age[0]) {
         oldest = people[0];
-        return oldest;
-    } else if (age[2] && age[0] < age[1]){
+
+    } else if (age[2] && age[0] < age[1]) {
         oldest = people[1];
-        return oldest;
-    };
+
+    } return oldest;
 };
 
 module.exports = findTheOldest;
