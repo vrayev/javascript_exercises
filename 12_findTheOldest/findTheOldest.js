1d2bb1556446f23bab801f466c0b5261e451071a
VR <vitrayev@gmail.com>
2021-09-13 12:53:09 -0400

    completed findTheOldest test, fixed loop boolean/fixed if-statement comparisons

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -4,21 +4,18 @@ const findTheOldest = function (people) {
     let age = new Array;
     let oldest;
     const currentYear = new Date().getFullYear();
-    for (i = 0; i < birthYear[i]; i++) {
+    for (i = 0; i < birthYear.length; i++) {
         if (typeof deathYear[i] === 'undefined') {
             age.push(currentYear - birthYear[i]);
         } else
             age.push(deathYear[i] - birthYear[i]);
     };
-    if (age[0] && age[1] < age[2]) {
+    if (age[0] < age[2] && age[1] < age[2]) {
         oldest = people[2];
-
-    } else if (age[1] && age[2] < age[0]) {
+    } else if (age[1] < age[0] && age[2] < age[0]) {
         oldest = people[0];
-
-    } else if (age[2] && age[0] < age[1]) {
+    } else if (age[2] < age[1] && age[0] < age[1]) {
         oldest = people[1];
-
     } return oldest;
 };
 
