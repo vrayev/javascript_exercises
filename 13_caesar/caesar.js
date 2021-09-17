9e3a3deb649c709075ff0f486652bb34e4e1d7d8
VR <vitrayev@gmail.com>
2021-09-17 14:22:47 -0400

    fixed bugs that happened in other test cases for caesar test

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -5,10 +5,10 @@ const caesar = function (inputString, modNumber) {
 
     //allows larger mod numbers
     if (modNumber > 25) {
-        modNumber = -(modNumber / 25);
+        modNumber = ((modNumber % 26) + 26) % 26;
     //allows larger negative mod numbers
     } else if (modNumber < -25) {
-        modNumber = ((modNumber - 25) / 25);
+        modNumber = ((modNumber % 26) + 26) % -26;
     }
     for (i = 0; i < inputString.length; i++) {
         if (inputString.charCodeAt(i) < 65) {
@@ -17,15 +17,21 @@ const caesar = function (inputString, modNumber) {
             charString = String.fromCharCode(charCodeNumber);
             finalWord = finalWord + charString;
         } else if (inputString.charCodeAt(i) >= 65) {
-            charCodeNumber = (inputString.charCodeAt(i) + modNumber)
+            //doesnt allow uppercase letters to become lowercase
+            //and viseversa
+            if (inputString.charCodeAt(i) >= 65 &&
+                inputString.charCodeAt(i) <= 90 &&
+                (inputString.charCodeAt(i) + modNumber) > 90) {
+                charCodeNumber = (inputString.charCodeAt(i) + modNumber - 26)
+            } else charCodeNumber = (inputString.charCodeAt(i) + modNumber)
             //only allows uppercase letters
-            if (charCodeNumber > 90 && charCodeNumber < 97) {
+            if (charCodeNumber >= 90 && charCodeNumber <= 97) {
                 charCodeNumber = charCodeNumber - 26;
             //only allows lowercase letters
-            } else if (charCodeNumber > 122) {
+            } else if (charCodeNumber >= 122 && charCodeNumber >= 97) {
                 charCodeNumber = charCodeNumber - 26;
             //allows negative mod numbers to wrap
-            } else if (modNumber < 0 && charCodeNumber < 65) {
+            } else if (modNumber < 0 && charCodeNumber <= 65) {
                 charCodeNumber = charCodeNumber + 26;
             }
             charString = String.fromCharCode(charCodeNumber);
